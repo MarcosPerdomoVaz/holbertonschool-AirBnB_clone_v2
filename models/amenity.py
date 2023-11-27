d62f2f500f42121eb065afe89fc828c98d68693b
MarcosPerdomoVaz <124619905+MarcosPerdomoVaz@users.noreply.github.com>
2023-11-26 21:41:30 -0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""State Module for HBNB project"""
 from models.base_model import BaseModel, Base
 from models.place import place_amenity
 from sqlalchemy import Column, String, Table, ForeignKey
@@ -8,7 +8,7 @@ from os import getenv
 
 
 class Amenity(BaseModel, Base):
-""" Amenitys of the Place"""
+"""Amenitys of the Place"""
 __tablename__ = "amenities"
 name = Column(String(128), nullable=False)
 
