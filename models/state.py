dd0e822b4216a983037cdeb5a2042e8c54d2b874
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2023-11-26 22:48:36 -0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -4,22 +4,8 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-
 class State(BaseModel, Base):
-    """ The state class, contains state name """
-    __tablename__ = "states"
-
+    """The state class, contains name."""
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-
-    if models.storage_type == 'db':
-        cities = relationship('City', backref='state', cascade='all, delete-orphan')
-    else:
-        @property
-        def cities(self):
-        """getter attribute cities that returns the list of City instances
-        with state_id equals to the current State.id"""
-        city_list = []
-        for city in list(models.storage.all(City).values()):
-        if city.state_id == self.id:
-        city_list.append(city)
-        return city_list
+    cities = relationship('City', back_populates='state', cascade='all, delete-orphan')
